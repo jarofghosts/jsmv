d6ea74fe5a8a5036dece99e7644bb675c1b876fc
jesse keane <me@jessekeane.me>
2015-02-21 15:20:16 -0800

    allow alias for require

--- test/index.js
+++ test/index.js
@@ -1,88 +1,134 @@
-var fs = require('fs')
+var path = require('path')
+  , fs = require('fs')
 
 var test = require('tape')
 
 var jsmv = require('../')
 
 fs.writeFileSync(
-    __dirname + '/testfile1.js'
-  , fs.readFileSync(__dirname + '/dummyfile.js')
+    path.join(__dirname, 'testfile1.js')
+  , fs.readFileSync(path.join(__dirname, 'dummyfile.js'))
 )
 
 fs.writeFileSync(
-    __dirname + '/testfile2.js'
-  , fs.readFileSync(__dirname + '/dummyfile2.js')
+    path.join(__dirname, 'testfile2.js')
+  , fs.readFileSync(path.join(__dirname, 'dummyfile2.js'))
 )
 
 fs.writeFileSync(
-    __dirname + '/testfile3.js'
-  , fs.readFileSync(__dirname + '/dummyfile.js')
+    path.join(__dirname, 'testfile3.js')
+  , fs.readFileSync(path.join(__dirname, 'dummyfile.js'))
 )
 
 fs.writeFileSync(
-    __dirname + '/testfile4.js'
-  , fs.readFileSync(__dirname + '/dummyfile3.js')
+    path.join(__dirname, 'testfile4.js')
+  , fs.readFileSync(path.join(__dirname, 'dummyfile3.js'))
+)
+
+fs.writeFileSync(
+    path.join(__dirname, 'testfile5.js')
+  , fs.readFileSync(path.join(__dirname, 'dummyfile4.js'))
 )
 
 test('change from named to relative module', function(t) {
-  var stream = jsmv({from: 'a', to: __dirname + '/b', relativeTo: true})
+  var stream = jsmv('a', path.join(__dirname, 'b'), {relativeTo: true})
 
-  stream.on('end', verify_change)
-  stream.write(__dirname + '/testfile1.js')
+  stream.on('end', verifyChange)
+  stream.write(path.join(__dirname, 'testfile1.js'))
 
-  function verify_change() {
-    var file_contents = fs.readFileSync(__dirname + '/testfile1.js', 'utf8')
-    fs.unlinkSync(__dirname + '/testfile1.js')
+  function verifyChange() {
+    var fileContents = fs.readFileSync(
+        path.join(__dirname, 'testfile1.js')
+      , 'utf8'
+    )
 
-    t.equal("var test = require('./b')\n", file_contents)
+    fs.unlinkSync(path.join(__dirname, 'testfile1.js'))
+
+    t.equal("var test = require('./b')\n", fileContents)
 
     t.end()
   }
 })
 
 test('rewrite works with named modules', function(t) {
-  var stream = jsmv({from: __dirname + '/a.js', to: 'b', relative_to: false})
+  var stream = jsmv(path.join(__dirname, 'a.js'), 'b', {relativeTo: false})
 
-  stream.on('end', verify_change)
-  stream.write(__dirname + '/testfile2.js')
+  stream.on('end', verifyChange)
+  stream.write(path.join(__dirname, 'testfile2.js'))
 
-  function verify_change() {
-    var file_contents = fs.readFileSync(__dirname + '/testfile2.js', 'utf8')
-    fs.unlinkSync(__dirname + '/testfile2.js')
+  function verifyChange() {
+    var fileContents = fs.readFileSync(
+        path.join(__dirname, 'testfile2.js')
+      , 'utf8'
+    )
 
-    t.equal("var test = require('b')\n", file_contents)
+    fs.unlinkSync(path.join(__dirname, 'testfile2.js'))
+
+    t.equal("var test = require('b')\n", fileContents)
 
     t.end()
   }
 })
 
 test('switches named modules', function(t) {
-  var stream = jsmv({from: 'a', to: 'b', relative_to: false})
+  var stream = jsmv('a', 'b', {relativeTo: false})
 
-  stream.on('end', verify_change)
-  stream.write(__dirname + '/testfile3.js')
+  stream.on('end', verifyChange)
+  stream.write(path.join(__dirname, 'testfile3.js'))
 
-  function verify_change() {
-    var file_contents = fs.readFileSync(__dirname + '/testfile3.js', 'utf8')
-    fs.unlinkSync(__dirname + '/testfile3.js')
+  function verifyChange() {
+    var fileContents = fs.readFileSync(
+        path.join(__dirname, 'testfile3.js')
+      , 'utf8'
+    )
 
-    t.equal("var test = require('b')\n", file_contents)
+    fs.unlinkSync(path.join(__dirname, 'testfile3.js'))
+
+    t.equal("var test = require('b')\n", fileContents)
 
     t.end()
   }
 })
 
 test('does not explode when js file has a shebang', function(t) {
-  var stream = jsmv({from: 'a', to: 'b', relative_to: false})
+  var stream = jsmv('a', 'b', {relativeTo: false})
 
-  stream.on('end', verify_change)
-  stream.write(__dirname + '/testfile4.js')
+  stream.on('end', verifyChange)
+  stream.write(path.join(__dirname, 'testfile4.js'))
 
-  function verify_change() {
-    var file_contents = fs.readFileSync(__dirname + '/testfile4.js', 'utf8')
-    fs.unlinkSync(__dirname + '/testfile4.js')
+  function verifyChange() {
+    var fileContents = fs.readFileSync(
+        path.join(__dirname, 'testfile4.js')
+      , 'utf8'
+    )
 
-    t.equal("#!/shebangs/shebangs\nvar test = require('b')\n", file_contents)
+    fs.unlinkSync(path.join(__dirname, 'testfile4.js'))
+
+    t.equal("#!/shebangs/shebangs\nvar test = require('b')\n", fileContents)
+
+    t.end()
+  }
+})
+
+test('can provide aliases', function(t) {
+  var stream = jsmv('a', 'b', {require: ['wutever', 'require']})
+
+  stream.on('end', verifyChange)
+  stream.write(path.join(__dirname, 'testfile5.js'))
+
+  function verifyChange() {
+    var fileContents = fs.readFileSync(
+        path.join(__dirname, 'testfile5.js')
+      , 'utf8'
+    )
+
+    fs.unlinkSync(path.join(__dirname, 'testfile5.js'))
+
+    t.equal(
+        "var test = wutever('b')\nvar herp = require('b')\n" + 
+            "var derp = nope('a')\n"
+      , fileContents
+    )
 
     t.end()
   }
