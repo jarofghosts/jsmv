0a897a3d66ab1f392c9a83c6edaa609c652bda80
jesse keane <me@jessekeane.me>
2015-02-22 01:24:18 -0800

    clean up a bit

--- index.js
+++ index.js
@@ -8,22 +8,24 @@ var select = require('cssauron-falafel')
   , falafel = require('falafel')
 
 var CWD = process.cwd()
+  , hasExtension = /\.js$/
+  , lazyRequire = /\/$/
+  , relative = /^\./
+  , shebang = /^#!/
 
 module.exports = jsmv
 
 function jsmv(from, to, _options) {
   var stream = through(parseFiles, Function())
     , options = _options || {}
-    , hasExtension = /\.js$/
-    , lazyRequire = /\/$/
-    , relative = /^\./
-    , shebang = /^#!/
     , started = false
     , files = []
 
-  var aliases = options.require || ['require']
+  var aliases = (options.require || ['require']).map(toSelector)
 
-  if(options.dir) CWD = path.resolve(options.dir)
+  if(options.dir) {
+    CWD = path.resolve(options.dir)
+  }
 
   return stream
 
@@ -44,9 +46,10 @@ function jsmv(from, to, _options) {
         }
 
         var hadShebang = false
-          , found = false
+          , processed
           , reqString
           , required
+          , deepRex
           , moveTo
           , quote
 
@@ -59,68 +62,66 @@ function jsmv(from, to, _options) {
 
         stream.emit('read', filename)
 
-        data = '' + falafel(data, parseNode)
+        processed = falafel(data, parseNode).toString()
 
-        if(!found) return next()
+        if(data === processed) {
+          return next()
+        }
 
         stream.queue(filename)
 
-        data = data.slice(hadShebang ? 17 : 15, -2)
-
-        fs.writeFile(path.resolve(CWD, filename), data, next)
+        processed = processed.slice(hadShebang ? 17 : 15, -2)
+        fs.writeFile(path.resolve(CWD, filename), processed, next)
 
         function parseNode(node) {
           required = isRequire(node)
 
-          if(!required) return
+          if(!required) {
+            return
+          }
 
           reqString = required.value
+
+          deepRex = new RegExp('^' + escape(from) + '/.+')
 
           if(relative.test(reqString)) {
             if(lazyRequire.test(reqString)) reqString += 'index.js'
             if(!hasExtension.test(reqString)) reqString += '.js'
 
             reqString = path.resolve(path.dirname(filename), reqString)
-          } else {
-              if(new RegExp('^' + escape(from) + '/.+').test(reqString) &&
-                  from !== reqString) {
-              if(options.force || options.forceFull) {
-                quote = node.source()[0]
-                found = true
-
-                moveTo = options.forceFull ?
-                  to :
-                  reqString.replace(new RegExp('^' + escape(from)), to)
-
-                node.update(quote + moveTo + quote)
-
-                return
-              }
-
-              stream.emit('conflict', {file: filename, string: reqString})
-
-              return
-            }
           }
 
-          if(from !== reqString) return
-
-          found = true
-
-          moveTo = options.relativeTo ?
-            path.relative(path.dirname(filename), to) :
-            to
-
-          if(options.relativeTo) {
-            if(!relative.test(moveTo)) {
-              moveTo = (/\//.test(moveTo) ? '.' : './') + moveTo
-            }
-
-            if(hasExtension.test(moveTo)) moveTo = moveTo.slice(0, -3)
-          }
+          if(from !== reqString && !deepRex.test(reqString)) return
 
           quote = node.source()[0]
-          node.update(quote + moveTo + quote)
+
+          if(from === reqString) {
+            moveTo = options.relativeTo ?
+              path.relative(path.dirname(filename), to) :
+              to
+
+            if(options.relativeTo) {
+              if(!relative.test(moveTo)) {
+                moveTo = (/\//.test(moveTo) ? '.' : './') + moveTo
+              }
+
+              if(hasExtension.test(moveTo)) {
+                moveTo = moveTo.slice(0, -3)
+              }
+            }
+
+            return node.update(quote + moveTo + quote)
+          }
+
+          if(options.force || options.forceFull) {
+            moveTo = options.forceFull ?
+              to :
+              reqString.replace(new RegExp('^' + escape(from)), to)
+
+            return node.update(quote + moveTo + quote)
+          }
+
+          stream.emit('conflict', {file: filename, string: reqString})
         }
       }
     }
@@ -135,16 +136,20 @@ function jsmv(from, to, _options) {
   }
 
   function isRequire(node) {
-    var selector
-      , result
-      , alias
+    var result
 
     for(var i = 0, len = aliases.length; i < len; ++i) {
-      alias = aliases[i]
-      selector = util.format('call > id[name=%s]:first-child + literal', alias)
-      result = select(selector)(node)
+      result = aliases[i](node)
 
-      if(result) return result
+      if(result) {
+        return result
+      }
     }
   }
+}
+
+function toSelector(alias) {
+  var selector = util.format('call > id[name=%s]:first-child + literal', alias)
+
+  return select(selector)
 }
